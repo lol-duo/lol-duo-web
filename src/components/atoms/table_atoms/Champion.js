e7e365a91c8482924d1d1e222281d651abb87743
imygnam <imygnam@gmail.com>
2022-10-30 12:42:57 +0900

    champion done

--- src/components/atoms/table_atoms/Champion.js
+++ src/components/atoms/table_atoms/Champion.js
@@ -1,6 +1,8 @@
 /** @jsxImportSource @emotion/react */
 
 import {css} from "@emotion/react";
+import fontList from "../../../assets/fontList";
+import colorList from "../../../assets/colorList";
 
 const cssList = {
     champion: {
@@ -28,6 +30,12 @@ const cssList = {
             width: "24px",
             height: "24px",
         }
+    },
+    name: {
+        ...fontList.roboto.bold["12"],
+        color: colorList.grayscale["000"],
+        height: "14px",
+        width: "87px",
     }
 }
 
@@ -67,6 +75,29 @@ const newCssList = {
             left: "75px",
             top: "46px",
         }
+    },
+    name: {
+        small: {
+            solo: {
+                position: "absolute",
+                left: "94px",
+                top: "16px",
+            },
+            duo: {
+                position: "relative",
+                textAlign: "center",
+                top: "2px",
+                left: "50%",
+                transform: "translateX(-50%)",
+            }
+        },
+        big: {
+            position: "relative",
+            textAlign: "center",
+            top: "2px",
+            left: "50%",
+            transform: "translateX(-50%)",
+        }
     }
 }
 
@@ -99,24 +130,49 @@ function PositionImage({src, size, newCss = {}}) {
     } src={src} alt={src}/>;
 }
 
-export function Champion({championInfo, size, newCss = {}}) {
+function ChampionName({name, newCss = {}}) {
+    return <div css={
+        css({
+            ...cssList.name,
+            ...newCss
+        })
+    }>{name}</div>;
+}
+
+export function Champion({championInfo, size, isSolo, newCss = {}}) {
     let championCss = size === "small" ?
         {
             champion: newCssList.champion.small,
             rune: newCssList.rune.small,
             position: newCssList.position.small,
+            name: isSolo ? newCssList.name.small.solo : newCssList.name.small.duo,
         } :
         {
             champion: newCssList.champion.big,
             rune: newCssList.rune.big,
             position: newCssList.position.big,
+            name: newCssList.name.big,
         };
 
     return (
-        <div>
-            <RuneImage src={championInfo.mainRuneImgUrl} newCss={championCss.rune}/>
-            <ChampionImage src={championInfo.championImgUrl} size={size} newCss={championCss.champion}/>
-            <PositionImage src={championInfo.positionImgUrl} size={size} newCss={championCss.position}/>
+        <div css={css({
+            position: "relative",
+            width: size === "small" ? isSolo ? "194px" : "87px" : "99px",
+            height: size === "small" ? isSolo ? "40px" : "20px" : "64px",
+            ...newCss
+        })}>
+            <div css={css({
+                position: "relative",
+                width: size === "small" ? "70px" : "99px",
+                height: size === "small" ? "46px" : "70px",
+                left: isSolo ? "0px" : "50%",
+                transform: isSolo ? "0" : "translateX(-50%)",
+            })}>
+                <RuneImage src={championInfo.mainRuneImgUrl} newCss={championCss.rune}/>
+                <ChampionImage src={championInfo.championImgUrl} size={size} newCss={championCss.champion}/>
+                <PositionImage src={championInfo.positionImgUrl} size={size} newCss={championCss.position}/>
+            </div>
+            <ChampionName name={championInfo.championName} newCss={championCss.name}/>
         </div>
     )
 }
--- src/components/atoms/table_atoms/Champion.stories.js
+++ src/components/atoms/table_atoms/Champion.stories.js
@@ -13,6 +13,8 @@ StoryChampion.args = {
         championImgUrl: "https://d2d4ci5rabfoyr.cloudfront.net/mainPage/champion/MissFortune.svg",
         mainRuneImgUrl: "https://d2d4ci5rabfoyr.cloudfront.net/mainPage/mainRune/LethalTempoTemp.svg",
         positionImgUrl: "https://d2d4ci5rabfoyr.cloudfront.net/mainPage/position/BOTTOM.svg",
+        championName: "미스 포츈",
     },
     size: "big",
+    isSolo: true,
 }
